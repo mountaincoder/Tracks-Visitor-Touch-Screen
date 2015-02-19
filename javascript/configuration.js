93642314d3319632cfd713956e8b87763b2cbfc6
mountaincoder <tony@zierniemann.com>
2015-02-19 09:48:30 -0700

    added link file

--- javascript/configuration.js
+++ javascript/configuration.js
@@ -1,3 +1,4 @@
-var SERVER_HREF = 'http://192.168.100.225';
+var SERVER_HREF = 'http://tracksremote.com';
+//var SERVER_HREF = 'http://192.168.100.225';
 var DETAIL_PAGE_TIMEOUT_MINUTES = 1;
 var FRESH_DATA_CHECK_INTERVAL_SECONDS = 30;
